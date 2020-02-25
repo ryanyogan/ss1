683195352b01b19ce58382b7c453df1724a2ead5
ryanyogan <ryanyogan@gmail.com>
2020-02-25 09:36:17 -0800

    implemented GetConsignments

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ const (
 
 type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
 }
 
 // Repository - Dummy repo, this simulates the use of a
@@ -33,6 +34,11 @@ func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, er
 	repo.consignments = updated
 	repo.mu.Unlock()
 	return consignment, nil
+}
+
+// GetAll consignments
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
 }
 
 // Service should implement all of the methods to satisfy the
@@ -56,6 +62,12 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 	// Return matching the `Response` message we created in our
 	// protobuf defintion.
 	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+// GetConsignments - returns all consignments, non-filtered, non-paginated
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignments: consignments}, nil
 }
 
 func main() {
